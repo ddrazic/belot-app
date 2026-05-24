4e338381ed1df913b93888b937f24aba11a5ab92
ddrazic <ddrazic@ffos.hr>
2026-05-24 16:39:44 +0200

    Updates

--- constants/game.ts
+++
@@ -1,7 +0,0 @@
-export const CALL_VALUES = [20, 50, 100, 150, 200];
-export const DEFAULT_TARGET_SCORE = 1001;
-export const DEFAULT_GAMES_COUNT = 0;
-export const TOTAL_POINTS_IN_ROUND = 162;
-export const STIGLJA_BONUS = 90;
-export const MAX_ZVANJE_COUNT = 3;
-export const LONG_PRESS_DELAY = 350;
